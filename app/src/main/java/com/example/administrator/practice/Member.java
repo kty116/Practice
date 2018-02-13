f10c0df12490090132394c8b73558ac739e8b893
kyoungae <kyoungae3017@gmail.com>
2018-02-13 12:09:35 +0900

    레트로핏, 롬복 라이브러리 추가

---
+++ app/src/main/java/com/example/administrator/practice/Member.java
@@ -0,0 +1,15 @@
+package com.example.administrator.practice;
+
+import android.support.v4.media.MediaMetadataCompat;
+
+import lombok.Data;
+
+/**
+ * Created by Administrator on 2018-02-13.
+ */
+
+@Data
+public class Member {
+    private String id;
+
+}
